c9e70292165248a9194e25fb696c9c3dead84f75
wuhaojie <kaitong.com>
2020-05-09 17:25:07 +0800

    update code

---
+++ src/main/java/com/wuhj/classLoder/MysqlTest.java
@@ -0,0 +1,9 @@
+package com.wuhj.classLoder;
+
+/**
+ * @author wuhaojie
+ * @ClassName MysqlTest
+ * @createTime 2020/5/7
+ */
+public class MysqlTest {
+}
---
+++ src/main/java/com/wuhj/classLoder/ThreadContextClassLoaderTest.java
@@ -0,0 +1,9 @@
+package com.wuhj.classLoder;
+
+/**
+ * @author wuhaojie
+ * @ClassName ThreadContextClassLoaderTest
+ * @createTime 2020/5/7
+ */
+public class ThreadContextClassLoaderTest {
+}
